7b4bb125fec7345ac26dc9e4bf71803d0232ad59
Alex <adkosmachev@edu.hse.ru>
2016-10-31 00:18:23 +0300

    Small fix

--- bin/launcher.py
+++ bin/launcher.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-import sys, os, shutil
+import sys, os, shutil, random
 
 # [type] [number]
 type = sys.argv[1]
 number = int(sys.argv[2])
 
 # change port in different modes in order to avoid situation, when OS doesn't close ports yet
-if type == "user":
-    ports = 8
-else:
-    ports = 9
+ports = random.randint(5000, 20000)
 
 open('mutex.txt', 'tw+').close()
 
@@ -20,7 +17,7 @@ os.mkdir('log')
 config = "{0} mutex.txt log/ {1}".format(type, number)
 
 for i in range(1, number+1):
-    config += " {0} 127.0.0.1 {1}".format(i, 1000*ports + i)
+    config += " {0} 127.0.0.1 {1}".format(i, ports + i)
 
 for i in range(1, number+1):
     os.system("./LMutex {0} {1} &".format(i, config))
